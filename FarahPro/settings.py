f35ff2d3bf5cb3fac476f3183e65868f513e2ddc
Meghdad <myarshia@gmail.com>
2025-08-13 23:12:28 +0330

    change DB Setting to PostgreSQL.

--- FarahPro/settings.py
+++ FarahPro/settings.py
@@ -94,10 +94,22 @@ WSGI_APPLICATION = 'FarahPro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'farahpro_db',
+        'USER': 'postgres',
+        'PASSWORD': 'R@cheljijali1',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
